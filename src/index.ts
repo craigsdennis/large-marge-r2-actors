bc1c56bcfcabceb5fc479b20fb562b182b365fbb
Craig Dennis <craigsdennis@gmail.com>
2025-07-01 23:09:58 +0700

    Adds resuming to the API

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,18 @@ const app = new Hono<SessionEnv & { Bindings: Env }>();
 
 app.use(useSession());
 
+app.get('/api/resume', async(c) => {
+	const data = await c.var.session.get();
+	if (data?.latestUploaderId) {
+		return c.json({
+			latestUploaderId: data.latestUploaderId,
+			lastUploadedFileName: data.lastUploadedFileName,
+			remainingCount: data.remainingCount,
+		});
+	}
+	return c.json({});
+})
+
 app.post('/api/uploads', async (c) => {
 	const payload = await c.req.json();
 	const uploaderId = c.env.UPLOADER.newUniqueId();
@@ -18,6 +30,7 @@ app.post('/api/uploads', async (c) => {
 	const data = await c.var.session.get();
 	if (data) {
 		data.latestUploaderId = uploaderId.toString();
+		data.lastUploadedFileName = payload.fileName;
 		await c.var.session.update(data);
 	}
 	return c.json({
@@ -36,13 +49,28 @@ app.get('/api/uploads/:id', async (c) => {
 	});
 });
 
+async function cleanup(c) {
+	await c.var.session.delete();
+}
+
 app.patch('/api/uploads/:id/:part_number', async (c) => {
 	// Get the Actor
 	const {id} = c.req.param();
 	const uploaderId = c.env.UPLOADER.idFromString(id);
 	const uploaderStub = c.env.UPLOADER.get(uploaderId);
 	// Pass request through into fetch
-	return await uploaderStub.fetch(c.req.raw);
+	const response = await uploaderStub.fetch(c.req.raw);
+	const {remainingCount} = await response.json<{remainingCount: number}>();
+	const data = await c.var.session.get();
+	if (data) {
+		if (remainingCount === 0) {
+			await cleanup(c);
+		} else {
+			data.remainingCount = remainingCount;
+			await c.var.session.update(data);
+		}
+	}
+	return c.json(remainingCount);
 });
 
 export default app;
